056529fe7c4e435e807f0ad8f37bd00ef68a2a5a
Sanam-Lama <slamatamang@stcloudstate.edu>
2019-05-07 23:34:33 -0400

    Update Main.java

--- src/com/slama/Main.java
+++ src/com/slama/Main.java
@@ -136,6 +136,31 @@ public class Main {
     }
 
 
+    /* Given a string and a non-empty word string, return a string made of each char just before and just after
+     * every appearance of the word in the string. Ignore cases where there is no char before or after the word,
+     * and a char may be included twice if it is between two words.
+
+     * wordEnds("abcXY123XYijk", "XY") → "c13i"
+     * wordEnds("XY123XY", "XY") → "13"
+     * wordEnds("XY1XY", "XY") → "11"
+     * */
+
+    public String wordEnds(String str, String word) {
+        String result = "";
+        int strLength = str.length();
+        int wordLength = word.length();
+
+        for(int i=0; i<strLength - wordLength + 1; i++) {
+            String temp = str.substring(i, i+wordLength);
+            if(i>0 && temp.equals(word)) {
+                result += str.substring(i-1, i);
+            }
+            if((i < (strLength - wordLength)) && temp.equals(word)) {
+                result += str.substring(i+wordLength, i+wordLength+1);
+            }
+        }
+        return result;
+    }
 
 
 }
